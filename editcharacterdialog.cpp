MarekDejak/DnD

commit 5c1b6fb73f607971877f5ea0f6c09827dbe67d07

separate card from editcharacterdialog, input proper skills

editcharacterdialog.cpp
@@ -9,56 +9,39 @@
 
 QVector<Character*> EditCharacterDialog::m_openCharacters = {};
 
-EditCharacterDialog::EditCharacterDialog(bool editable, Character* character, QWidget* parent)
-    : QDialog(parent), m_editable(editable), m_character(character) {
+EditCharacterDialog::EditCharacterDialog(Character* character, QWidget* parent)
+    : QDialog(parent), m_character(character) {
     manageOpenDialogs();
     setupTitleAndGeometry();
     populateLayout();
     show();
 }
 
-template<class Editor>
 void EditCharacterDialog::generateFixedLayout(QFormLayout* formLayout) {
-    auto* nameEdit = new Editor(this);
+    auto* nameEdit = new QLineEdit(this);
     nameEdit->setText(m_character->name());
     formLayout->addRow(nameLabel, nameEdit);
 
-    auto* raceEdit = new Editor(this);
+    auto* raceEdit = new QLineEdit(this);
     raceEdit->setText(m_character->getRace());
     formLayout->addRow(raceLabel, raceEdit);
 
-    auto* professionEdit = new Editor(this);
+    auto* professionEdit = new QLineEdit(this);
     professionEdit->setText(m_character->getProfession());
     formLayout->addRow(professionLabel, professionEdit);
-
-    if constexpr (std::is_same_v<QLineEdit, Editor>) {
-        connect(nameEdit, &Editor::editingFinished, this, [=]() { m_character->setName(nameEdit->text()); });
-        connect(professionEdit, &Editor::editingFinished, this,
-                [=]() { m_character->setProfession(professionEdit->text()); });
-        connect(raceEdit, &Editor::editingFinished, this, [=]() { m_character->setRace(raceEdit->text()); });
-    }
 }
 
 void EditCharacterDialog::populateLayout() {
     auto* formLayout = new QFormLayout(this);
 
-    if (m_editable) {
-        generateFixedLayout<QLineEdit>(formLayout);
-        for (const auto [ability, value] : m_character->getAllAbilities()) {
-            auto* spinBox = new QSpinBox(this);
-            spinBox->setValue(value);
-            const auto abilityName = ability;
-            connect(spinBox, &QSpinBox::editingFinished, this,
-                    [=]() { m_character->setAbility(abilityName, spinBox->value()); });
-            formLayout->addRow(ability, spinBox);
-        }
-    } else {
-        generateFixedLayout<QLabel>(formLayout);
-        for (const auto [ability, value] : m_character->getAllAbilities()) {
-            auto* label = new QLabel(this);
-            label->setText(QString::number(value));
-            formLayout->addRow(ability, label);
-        }
+    generateFixedLayout(formLayout);
+    for (const auto [ability, value] : m_character->getAllAbilities()) {
+        auto* spinBox = new QSpinBox(this);
+        spinBox->setValue(value);
+        const auto abilityName = ability;
+        connect(spinBox, &QSpinBox::editingFinished, this,
+                [=]() { m_character->setAbility(abilityName, spinBox->value()); });
+        formLayout->addRow(ability, spinBox);
     }
 
     auto buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok, this);

card.h
@@ -1,48 +1,21 @@
 #pragma once
-#include "CharacterInfo.h"
-#include "Ptaszki.h"
+
+#include "character.h"
 
 #include <QDialog>
-#include <QVector>
-#include <QCheckBox>
-#include <unordered_set>
-#include <set>
+#include <QFormLayout>
 
-
-namespace Ui {
-class Card;
-}
-
-class Card : public QDialog
-{
-    Q_OBJECT
-
+class Card : public QDialog {
 public:
-    explicit Card(QWidget *parent = nullptr);
-    void setCharacterInfo(CharacterInfo info);
-    void setName(QString imie);
-    void setPtaszki(Ptaszki ptaszki);
-
-    Ptaszki getPtaszki();//bylo na koncu const
-
-    ~Card();
+    Card(Character* character, QWidget* parent);
 
 private:
-    Ui::Card *ui;
-    QVector<Player> m_characters;
-    QVector<QString> m_chosenCharacters;
-    QString m_name;
-    Ptaszki m_ptaszki;
+    void generateFixedLayout(QFormLayout* layout);
+    void populateLayout();
+    void manageOpenDialogs();
+    void setupTitleAndGeometry();
 
-    std::set<QCheckBox*> m_setPtaszkow;
-    std::set<QString> m_setNazwSkilli;
-
-    void reloadCardInfo(QVector<Player> cardInfo);
-
-
-
-
-    void onCloseClicked();
-    void onLvlUpClicked();
+    bool m_editable;
+    Character* m_character;
+    static QVector<Character*> m_openCharacters;
 };
-

editcharacterdialog.h
@@ -7,16 +7,14 @@
 
 class EditCharacterDialog : public QDialog {
 public:
-    EditCharacterDialog(bool editable, Character* character, QWidget* parent);
+    EditCharacterDialog(Character* character, QWidget* parent);
 
 private:
-    template<class Editor>
     void generateFixedLayout(QFormLayout* layout);
     void populateLayout();
     void manageOpenDialogs();
     void setupTitleAndGeometry();
 
-    bool m_editable;
     Character* m_character;
     static QVector<Character*> m_openCharacters;
 };

mapwidget.cpp
@@ -33,11 +33,7 @@ MapWidget::MapWidget(CharacterModel* model, QWidget* parent) : QWidget(parent), 
 
 void MapWidget::onPrzyciskClicked(const QModelIndex& index) {
     auto* character = m_model->data(index, CharacterModel::CharacterRole).value<Character*>();
-    new EditCharacterDialog(false, character, this);
-}
-
-void MapWidget::setPtaszki(Ptaszki input) {
-    m_ptaszki = input;
+    new Card(character, this);
 }
 
 void MapWidget::onDataChanged() {

mapwidget.h
@@ -17,7 +17,6 @@ class MapWidget : public QWidget {
 
 public:
     explicit MapWidget(CharacterModel* model, QWidget* parent = nullptr);
-    void setPtaszki(Ptaszki input);
 
 signals:
     void stopPressed();
@@ -30,7 +29,6 @@ private:
     QVBoxLayout* m_verticalLayout;
     Mapa* m_mapa;
     CharacterInfo m_info;
-    Ptaszki m_ptaszki;
     QVector<QString> m_openCards;
 
 private slots:

selectcharacter.cpp
@@ -85,8 +85,8 @@ void SelectCharacter::addNewCharacter() {
 void SelectCharacter::editCharacter() {
     const auto selection = m_currentListView->selectionModel()->selection();
     if (selection.count() == 1) {
-        new EditCharacterDialog(
-            true, selection.indexes().first().data(CharacterModel::CharacterRole).value<Character*>(), this);
+        new EditCharacterDialog(selection.indexes().first().data(CharacterModel::CharacterRole).value<Character*>(),
+                                this);
     }
 }
 

commit 60757b32409a249c4a479955533c9baf010333f7

fix bug introduced in 5c1b6fb7 (separate card from editcharacterdialog)

editcharacterdialog.cpp
@@ -29,6 +29,11 @@ void EditCharacterDialog::generateFixedLayout(QFormLayout* formLayout) {
     auto* professionEdit = new QLineEdit(this);
     professionEdit->setText(m_character->getProfession());
     formLayout->addRow(professionLabel, professionEdit);
+
+    connect(nameEdit, &QLineEdit::editingFinished, this, [=]() { m_character->setName(nameEdit->text()); });
+    connect(professionEdit, &QLineEdit::editingFinished, this,
+            [=]() { m_character->setProfession(professionEdit->text()); });
+    connect(raceEdit, &QLineEdit::editingFinished, this, [=]() { m_character->setRace(raceEdit->text()); });
 }
 
 void EditCharacterDialog::populateLayout() {